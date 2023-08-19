a8a1b1be7ec01a4bce73d4e6e7b1f78cb30b2f82
Alton Phan <altonphan@outlook.com>
2023-08-18 22:56:20 -0500

    feat: add remaining sections, new images, mobile responsiveness

---
+++ dist/script.js
@@ -0,0 +1,31 @@
+// script.js
+
+// Function to generate a random color
+function getRandomColor() {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+  }
+  
+  // Function to change the frog background color
+  function changeFrogColor() {
+    const frogContainer = document.getElementById("frogContainer");
+    frogContainer.style.backgroundColor = getRandomColor();
+  }
+  
+  // Add event listeners to the frog image container
+  document.addEventListener("DOMContentLoaded", function() {
+    const frogContainer = document.getElementById("frogContainer");
+    const clickMeText = document.getElementById("clickMeText");
+  
+    frogContainer.addEventListener("click", changeFrogColor);
+  
+    frogContainer.addEventListener("mouseenter", function() {
+      clickMeText.style.opacity = 1;
+      frogContainer.style.filter = "brightness(70%)";
+    });
+  
+    frogContainer.addEventListener("mouseleave", function() {
+      clickMeText.style.opacity = 0;
+      frogContainer.style.filter = "brightness(100%)";
+    });
+  });
+  
